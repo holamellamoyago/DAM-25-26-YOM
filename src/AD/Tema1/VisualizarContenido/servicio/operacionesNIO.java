f6660cb5915f44ce5711adb01e223f84db39ff95
Yago Otero <yagootero2003@gmail.com>
2025-09-23 09:56:27 +0200

    Ejercicios acceso a datos

--- src/AD/Tema1/VisualizarContenido/servicio/operacionesNIO.java
+++ src/AD/Tema1/VisualizarContenido/servicio/operacionesNIO.java
@@ -2,6 +2,7 @@ package AD.Tema1.VisualizarContenido.servicio;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.AccessDeniedException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,17 +10,134 @@ import java.nio.file.Paths;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Stream;
+
+/*
+ * Para cada Walk hay que poner un try que analice si 
+ *  Files.isRegularFile && 
+ *  Files.isreadable
+ */
 
 public class operacionesNIO {
     public static void main(String[] args) {
         // visualizarContenido("src");
         // recorrerRecursivo("src");
         // filtrarPorExtension("src", ".java");
-        recorrerRecursivofuncional("src");
+        // recorrerRecursivofuncional("src");
+        // filtrarPorExtensionFuncional("src/", ".txt");
+        // filtrarPorSubcadena("src/", "xt");
+        filtrarPorExtensionYOrdenar("src/", ".txt");
+
+        // filtrarPorExtensionRecursivoFuncional(".", ".txt");
+    }
+
+    private static void copiarArchivo(String origen, String destino) {
+        Path archivo = Paths.get(origen);
+
+        if (Files.isDirectory(archivo)) {
+            throw new ArithmeticException("aa");
+        }
+        ;
+
+    }
+
+    public static void filtrarPorExtensionYOrdenar(String ruta, String ext) {
+        Path dir = Paths.get(ruta);
+
+        try (Stream<Path> stream = Files.list(dir)) {
+
+            Stream<Path> filtrados = stream.filter(t -> t.getFileName().toString().endsWith(ext));
+            filtrados.forEach(t -> {
+                System.out.println(t.getFileName());
+            });
+
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        // El segundo parámetro de DirectoryStream es de comodines
+        // try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, "*" + ext))
+        // {
+
+        // // Comparator<Path> comp = Comparator.comparing(t -> t.get );
+        // // Comparator<Path> comp = Comparator.comparing(t -> t.getFileName().)
+
+        // // Comparator<Path> comp1 = Comparator.comparing((p1, p2) -> p1. )
+
+        // Comparator<Path> comp = new Comparator<Path>() {
+        // @Override
+        // public int compare(Path o1, Path o2) {
+        // return o1.getFileName().toString().compareTo(o2.getFileName().toString());
+        // }
+        // };
+
+        // for (Path path : stream) {
+        // System.out.println(path.getFileName().toString());
+        // }
+        // } catch (Exception e) {
+        // e.printStackTrace();
+        // }
+
+    }
+
+    public static List<Path> filtrarPorSubcadena(String ruta, String subcadena) {
+        Path dir = Paths.get(ruta);
+        ArrayList<Path> totalPaths = new ArrayList<>();
+
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)) {
+
+            stream.forEach(t -> {
+                if (t.getFileName().toString().contains(subcadena)) {
+                    totalPaths.add(t);
+                }
+            });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(totalPaths);
+        return totalPaths;
+    }
+
+    public static void filtrarPorExtensionFuncional(String ruta, String ext) {
+        Path dir = Paths.get(ruta);
+
+        // El segundo parámetro de DirectoryStream es de comodines
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, "*" + ext)) {
+            for (Path path : stream) {
+                System.out.println(path.getFileName().toString());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static List<Path> filtrarPorExtensionRecursivoFuncional(String ruta, String ext) {
+        ArrayList<Path> totalPaths = new ArrayList<>();
+        Path dir = Paths.get(ruta);
+
+        try (Stream<Path> files = Files.walk(dir)) {
+
+            totalPaths.addAll(files.filter(t -> t.getFileName().toString().endsWith(ext)).toList());
+
+            System.out.println(totalPaths);
+
+            files.close();
+        } catch (AccessDeniedException e) {
+            System.err.println("Acceso denegado en " + dir);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return totalPaths;
+
     }
 
     public static List<Path> recorrerRecursivofuncional(String ruta) {
@@ -34,6 +152,7 @@ public class operacionesNIO {
                 String frase = sangria + p.getFileName();
 
                 System.out.println(frase);
+                totalPaths.add(p);
             });
 
             files.close();
