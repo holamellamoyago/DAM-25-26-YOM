1fa9a5de13d686368bb162b45bf8c2a613190193
Yago Otero <yagootero2003@gmail.com>
2025-10-09 14:18:16 +0200

    Ejercicios AD

--- src/AD/Tema1/Actividad3Corregido/persistencia/RandomFile.java
+++ src/AD/Tema1/Actividad3Corregido/persistencia/RandomFile.java
@@ -5,6 +5,9 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.io.UnsupportedEncodingException;
+import java.time.LocalDate;
+import java.util.Set;
+import java.util.TreeSet;
 
 import AD.Tema1.Actividad3Corregido.clases.Equipo;
 import AD.Tema1.Actividad3Corregido.clases.Patrocinador;
@@ -56,15 +59,13 @@ public class RandomFile extends Archivo {
         return (int) Math.ceil((double) rdmFile.length() / ESPACIO_INDIVIDUAL);
     }
 
-    public int cogerIdUltimoEquipo() throws IOException {
-        return String.valueOf(cogerSiguientePosicion()).toCharArray()[0];
-    }
-
     private void guardarCamposEquipo(Equipo e) throws IOException {
         rdmFile.writeInt(e.getIdEquipo());
         rdmFile.writeInt(e.getNumPatrocinadores());
         rdmFile.writeUTF(e.getNombre());
         rdmFile.writeBoolean(e.isBorrado());
+
+        // rdmFile.writeUTF(ruta);
     }
 
     private boolean calcularTamanhoEquipo(Equipo e) throws UnsupportedEncodingException{
@@ -106,6 +107,48 @@ public class RandomFile extends Archivo {
                 e.printStackTrace();
             }
         }
+    }
+
+    // private Equipo() {
+    //     (int) Math.ceil(double) raf.length() / TAMAÑo_REGISTRO
+    // }
+
+    public Equipo cogerEquipo(int id) throws IOException{
+        Equipo e;
+
+        if (id > cogerUltPosicion()) {
+            return null;
+        }
+
+        int posicionEquipo = (int) Math.ceil((double) rdmFile.length() / ESPACIO_INDIVIDUAL);
+        rdmFile.seek(posicionEquipo);
+
+        int id2 = rdmFile.readInt();
+        int numPatrocinadores = rdmFile.readInt();
+        String nombreEquipo = rdmFile.readUTF();
+        boolean borrado = rdmFile.readBoolean();
+
+        if (borrado) {
+            return null;
+        }
+
+        Set<Patrocinador> patrocinadores = new TreeSet<>();
+        for (int i = 0; i < numPatrocinadores; i++) {
+            int donacion = rdmFile.readInt();
+            String fecha = rdmFile.readUTF();
+            String nombrePatrocinador = rdmFile.readUTF();
+
+            LocalDate fechaParseada = LocalDate.parse(fecha);
+            patrocinadores.add(new Patrocinador(nombrePatrocinador, donacion, fechaParseada));
+        }
+
+        e = new Equipo(nombreEquipo, patrocinadores);
+        
+        return e;
+    }
+
+    private int cogerUltPosicion() throws IOException{
+        return (int) Math.ceil((double) rdmFile.length() / ESPACIO_INDIVIDUAL);
     }
 
 }
