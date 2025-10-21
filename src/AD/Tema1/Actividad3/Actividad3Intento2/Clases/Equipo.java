745ce673dbfe3bbafb6316f7b93f9919cb5a781f
Yago Otero <Yagootero2003@gmail.com>
2025-10-21 22:55:36 +0200

    Deberes día 21-10

---
+++ src/AD/Tema1/Actividad3/Actividad3Intento2/Clases/Equipo.java
@@ -0,0 +1,91 @@
+package AD.Tema1.Actividad3.Actividad3Intento2.Clases;
+
+import java.util.Set;
+
+public class Equipo {
+    private int idEquipo, numPatrocinadores;
+    private String nombre;
+    private boolean borrado;
+
+    Set<Patrocinador> patrocinadores;
+
+    public Equipo(String nombre, Set<Patrocinador> patrocinadores) {
+        this.nombre = nombre;
+        this.patrocinadores = patrocinadores;
+        this.borrado = false;
+        this.numPatrocinadores = patrocinadores.size();
+    }
+
+    public int getIdEquipo() {
+        return idEquipo;
+    }
+
+    public void setIdEquipo(int idEquipo) {
+        this.idEquipo = idEquipo;
+    }
+
+    public int getNumPatrocinadores() {
+        return numPatrocinadores;
+    }
+
+    public void setNumPatrocinadores(int numPatrocinadores) {
+        this.numPatrocinadores = numPatrocinadores;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isBorrado() {
+        return borrado;
+    }
+
+    public void setBorrado(boolean borrado) {
+        this.borrado = borrado;
+    }
+
+    public Set<Patrocinador> getPatrocinadores() {
+        return patrocinadores;
+    }
+
+    public void setPatrocinadores(Set<Patrocinador> patrocinadores) {
+        this.patrocinadores = patrocinadores;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Equipo other = (Equipo) obj;
+        if (nombre == null) {
+            if (other.nombre != null)
+                return false;
+        } else if (!nombre.equals(other.nombre))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipo " + idEquipo + "(" + nombre + "), numPatrocinadores=" + numPatrocinadores;
+    }
+
+    
+
+}
---
+++ src/AD/Tema1/Actividad3/Actividad3Intento2/Persistencia/Archivo.java
@@ -0,0 +1,26 @@
+package AD.Tema1.Actividad3.Actividad3Intento2.Persistencia;
+
+import java.io.File;
+
+public abstract class Archivo {
+    File file;
+    
+    public Archivo(String ruta) {
+        this.file = new File(ruta);
+    }
+    
+    public abstract void abrirarchivo();
+    public abstract void cerararchivo();
+
+    public boolean existe(){
+        if (file.exists()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean eliminarArchivo(){
+        return file.delete();
+    }
+}
