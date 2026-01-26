b01b6e8f9e6a25bea2f7954666fbae36579f33a2
Yago Otero <yagootero2003@gmail.com>
2026-01-26 15:11:52 +0100

    Avance en pescadores

---
+++ DAM-25-26/src/PSP/Tema2/Menu/Principal.java
@@ -0,0 +1,30 @@
+package PSP.Tema2.Menu;
+
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int opcion;
+        while (true) {
+            System.out.println("MENU");
+            System.out.println("\t1 -> Opción 1");
+
+            System.out.println("\nEscribe tú opción: ");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+
+                    break;
+
+                default:
+                    System.out.println("\nSelecciona una opción correcta");
+                    break;
+            }
+
+        }
+
+    }
+}
